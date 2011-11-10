a669d82b048abc92be2bb04d1aa911a9d996d791
chustar <cnnaji2003@gmail.com>
2011-11-10 14:24:12 -0800

    Added unit test

--- user_exec.cpp
+++ user_exec.cpp
@@ -2,33 +2,77 @@
 
 using namespace std;
 
-int user_exec(vector<block> var) {
+int exec_env = 0;
+int negate = false;
+
+int user_exec(vector<string> var), vector<char> types {
 	int child = 0;
 	for(int i = 0; i < var.length(); i++) {
-		if(is_token(var[i])) {
+		if(types[i] == 'O') {
 			resolve_exec();
 			set_exec(var[i]);
 		} else if(is_cmd(var[i])) {
 			child = fork_exec_bg(var[i].str);
-		}
-		else {
+		} else {
 			resolve_exec(child);
 		}
 	}
+	resolve_exec(child);
 }
 
-bool is_token(block v) {
-
+bool is_token(string v) {
+	
 }
 
-bool set_exec(block v) {
+bool set_exec(string v) {
+	int res = 0;
+	switch(v.type) {
+		case NOT:
+			negate = false;
+		break;
+		case AND:
+			res = wait(child);
+			if (res == 0 || negate) {
+				
+			} else if(res != 0 || negate) {
 
+			}
+		break;
+		case OR:
+
+		break;
+		
+	}
 }
 
-int fork_exec_bg(block v) {
+int fork_exec_bg(string v, char type) {
 
 }
 
 bool resolve_exec(int child) {
+	if(child != 0) {
+		switch(exec_env) {
+			case AND:
 
+			break;
+			case OR:
+
+			break;
+			case STREAM_IN:
+
+			break;
+			case STREAM_OUT:
+
+			break;
+			case STREAM_ERR:
+
+			break;
+			case PIPE:
+
+			break;
+			case BACKGROUND:
+
+			break;
+		}
+	}
 }
