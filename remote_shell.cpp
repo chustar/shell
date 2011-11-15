ba47ba2a98fb8af263de19d967e7614940be61a3
ahruizguerra <abraham@ahruizguerra.net>
2011-11-15 12:35:29 -0800

    remote shell code

---
+++ remote_shell.cpp
@@ -0,0 +1,10 @@
+#include "remote_shell.h"
+
+
+int remote_shell(string name, string command)
+{
+cout << endl << "name " << name << endl;
+cout << "command " << command << endl;
+
+
+}
---
+++ remote_shell.h
@@ -0,0 +1,10 @@
+#ifndef REMOTE_SHELL_H
+#define REMOTE_SHELL_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int remote_shell(string name, string command);
+
+#endif
