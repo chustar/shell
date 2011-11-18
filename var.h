ba6215a9c6aba65dfdcba6595b7a8f93c7c22493
chustar <cnnaji2003@gmail.com>
2011-11-18 10:45:50 -0800

    added data file parsing

--- var.h
+++ var.h
@@ -3,7 +3,8 @@ using namespace std;
 #ifndef VAR_H
 #define VAR_H
 int load_vars();
-string find_replace_vars(string&);
+int find_replace_files(string&);
+int find_replace_vars(string&);
 string get_var(string);
 int set_var(string, string);
 #endif
